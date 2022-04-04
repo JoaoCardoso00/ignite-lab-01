a91ea0bfc7261451b559ce6d6e9c73d6ef6a8c2e
JoaoCardoso00 <jvcq13@gmail.com>
2022-04-04 16:34:19 -0300

    initial commit

---
+++ src/http/http.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TestController} from './test.controller'
+
+//process.env
+
+@Module({
+    imports: [ConfigModule.forRoot()],
+    controllers: [TestController],
+})
+export class HttpModule {}
